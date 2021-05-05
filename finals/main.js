15630923a8c9e99ede0f161b536facd49f1a12ac
Alle0944 <78514151+Alle0944@users.noreply.github.com>
2021-05-05 18:42:24 -0500

    Add files via upload

---
+++ finals/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+$('.backtotop').click(function() {
+	$('body,html').animate({scrollTop: 0}, 900);
+});
+
+$('.menuhome').click(function() {
+	$('body,html').animate({scrollTop: 0}, 900);
+});
+
+$('.menuearly').click(function() {
+	$('body,html').animate({scrollTop: 834}, 900);
+});
+
+$('.menueighties').click(function() {
+	$('body,html').animate({scrollTop: 1668}, 900);
+});
+
+$('.menunineties').click(function() {
+	$('body,html').animate({scrollTop: 2900}, 900);
+});
+
+$('.menutwothousands').click(function() {
+	$('body,html').animate({scrollTop: 4132}, 900);
+});
+
+$('.menutwentytens').click(function() {
+	$('body,html').animate({scrollTop: 5364}, 900);
+});
+
+
+
+});
